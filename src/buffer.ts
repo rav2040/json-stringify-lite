26671804cd3f7c0934a2ee836f363b34c6db36f5
Stuart Kennedy <rav2040@gmail.com>
2020-03-22 15:25:32 +1300

    Refactor serializeBuffer()

--- src/buffer.ts
+++ src/buffer.ts
@@ -1,14 +1,15 @@
 /**
- * stringifyBuffer() converts the provided buffer to the same JSON string that would result if converted with
- * JSON.stringify(). Instead of calling buf.toJSON() and then stringifying the result, the buffer values are
- * concatenated with a preexisting string that matches what would be returned by JSON.stringify().
+ * Converts the provided buffer to the same JSON string that would result if converted with JSON.stringify(). Instead
+ * of calling toJSON() and then stringifying the result, the buffer values are concatenated with a pre-prepared string
+ * that matches the output of JSON.stringify().
  */
 
-export function stringifyBuffer(buf: Buffer): string {
+export function serializeBuffer(buf: Buffer): string {
   let str = '{"type":"Buffer","data":[';
+  let i;
   let prefix = '';
 
-  for (let i = 0; i < buf.length; i++) {
+  for (i = 0; i < buf.length; i++) {
     str += prefix + buf[i];
     prefix = ',';
   }
